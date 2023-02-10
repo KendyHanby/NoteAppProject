23d4ab3ffbc6a9375f1dde79a9b4cb7f7a4c2613
moonegg <moonegg2022@gmail.com>
2023-02-10 19:29:05 +0630

    TabIconSelectedColorTest

--- app/src/main/java/com/sxi/notes/MainActivity.java
+++ app/src/main/java/com/sxi/notes/MainActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.tabs.TabLayout;
 import com.sxi.notes.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -43,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
                 binding.tabTool.setVisibility(View.GONE);
             }
         });
+        //Icon selected color
+        setupTabIcons();
 
         //Building SQLiteDatabase
        mySqlHelper = new MySqlHelper(getApplicationContext());
@@ -101,4 +106,30 @@ public class MainActivity extends AppCompatActivity {
            }
 
      */
+    private void setupTabIcons() {
+
+
+
+        binding.tab.getTabAt(0).getIcon().setColorFilter(Color.GREEN, PorterDuff.Mode.SRC_IN);
+        binding.tab.getTabAt(1).getIcon().setColorFilter(Color.parseColor("#a8a8a8"), PorterDuff.Mode.SRC_IN);
+
+
+        binding.tab.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                tab.getIcon().setColorFilter(getColor(R.color.selected_color), PorterDuff.Mode.SRC_IN);
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                tab.getIcon().setColorFilter(getColor(R.color.unselected_color), PorterDuff.Mode.SRC_IN);
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
 }
