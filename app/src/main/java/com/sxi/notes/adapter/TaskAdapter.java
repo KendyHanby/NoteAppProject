0d0dba4d1feee1643af46da93f38f4c3b0b97cec
Kunteinzo <theinhtetoo473@gmail.com>
2023-02-12 14:19:32 +0630

    need to fix adapter and layout

--- app/src/main/java/com/sxi/notes/adapter/TaskAdapter.java
+++ app/src/main/java/com/sxi/notes/adapter/TaskAdapter.java
@@ -1,9 +1,12 @@
 package com.sxi.notes.adapter;
 
+import android.text.SpannableString;
+import android.text.style.StrikethroughSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -21,7 +24,15 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskVH> {
 
     @Override
     public void onBindViewHolder(@NonNull TaskVH holder, int position) {
-
+        holder.check.setOnCheckedChangeListener((compoundButton, b) -> {
+            SpannableString string = new SpannableString(holder.title.getText().toString());
+            if (b){
+                string.setSpan(new StrikethroughSpan(),0,holder.title.getText().length(),0);
+            } else {
+                string.removeSpan(new StrikethroughSpan());
+            }
+            holder.title.setText(string);
+        });
     }
 
     @Override
@@ -32,10 +43,12 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskVH> {
     static class TaskVH extends RecyclerView.ViewHolder {
         CheckBox check;
         TextView title;
+        RecyclerView listTask;
         public TaskVH(@NonNull View itemView) {
             super(itemView);
             check = itemView.findViewById(R.id.task_check);
             title = itemView.findViewById(R.id.task_title);
+            listTask = itemView.findViewById(R.id.list_task);
         }
     }
 }
