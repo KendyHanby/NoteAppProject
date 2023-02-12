546aa4eb63bd0f8abf4e79051aa6e06eedf76f4f
Kunteinzo <theinhtetoo473@gmail.com>
2023-02-12 10:42:58 +0630

    config the task

--- app/src/main/java/com/sxi/notes/adapter/TaskAdapter.java
+++ app/src/main/java/com/sxi/notes/adapter/TaskAdapter.java
@@ -1,16 +1,22 @@
 package com.sxi.notes.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.sxi.notes.R;
 
 public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskVH> {
     @NonNull
     @Override
     public TaskVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new TaskVH(LayoutInflater.from(parent.getContext()).inflate(R.layout.list_task_item,parent,false));
     }
 
     @Override
@@ -20,12 +26,16 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskVH> {
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 10;
     }
 
-    class TaskVH extends RecyclerView.ViewHolder {
+    static class TaskVH extends RecyclerView.ViewHolder {
+        CheckBox check;
+        TextView title;
         public TaskVH(@NonNull View itemView) {
             super(itemView);
+            check = itemView.findViewById(R.id.task_check);
+            title = itemView.findViewById(R.id.task_title);
         }
     }
 }
