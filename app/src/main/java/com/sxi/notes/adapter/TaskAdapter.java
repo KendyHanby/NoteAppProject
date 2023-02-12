6503cf3af1cc8e08c8b7487e4d68bb7ef6dcd49e
Kunteinzo <theinhtetoo473@gmail.com>
2023-02-12 16:13:20 +0630

    not ok yet

--- app/src/main/java/com/sxi/notes/adapter/TaskAdapter.java
+++ app/src/main/java/com/sxi/notes/adapter/TaskAdapter.java
@@ -1,11 +1,77 @@
 package com.sxi.notes.adapter;
 
-import android.database.DataSetObserver;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ExpandableListAdapter;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.CheckBox;
 
-public class TaskAdapter {
-    public void jj(){
+import com.sxi.notes.R;
+
+public class TaskAdapter extends BaseExpandableListAdapter {
+
+    private final Context context;
+
+    public TaskAdapter(Context requireContext) {
+        context = requireContext;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return 5;
+    }
+
+    @Override
+    public int getChildrenCount(int i) {
+        return 3;
+    }
+
+    @Override
+    public Object getGroup(int i) {
+        return null;
+    }
+
+    @Override
+    public Object getChild(int i, int i1) {
+        return null;
+    }
+
+    @Override
+    public long getGroupId(int i) {
+        return 0;
+    }
+
+    @Override
+    public long getChildId(int i, int i1) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+        View group = view;
+        if (view == null) {
+            group = LayoutInflater.from(context).inflate(R.layout.list_task_item, viewGroup, false);
+        }
+        return group;
+    }
+
+    @Override
+    public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
+        View child = view;
+        if (view == null) {
+            child = LayoutInflater.from(context).inflate(R.layout.list_subtask_item, viewGroup, false);
+        }
+        return child;
+    }
+
+    @Override
+    public boolean isChildSelectable(int i, int i1) {
+        return true;
     }
 }
--- app/src/main/java/com/sxi/notes/ui/TaskFragment.java
+++ app/src/main/java/com/sxi/notes/ui/TaskFragment.java
@@ -1,19 +1,13 @@
 package com.sxi.notes.ui;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
+
+import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.sxi.notes.R;
 import com.sxi.notes.adapter.TaskAdapter;
@@ -36,18 +30,13 @@ public class TaskFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentTaskBinding.inflate(inflater,container,false);
-        binding.listTask.setHasFixedSize(true);
-        binding.listTask.setLayoutManager(new LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL,false));
-//        binding.listTask.setAdapter(new TaskAdapter());
+
+
+        binding.listTask.setAdapter(new TaskAdapter(requireContext()));
 
         FloatingActionButton mainFab = requireActivity().findViewById(R.id.main_fab);
         mainFab.setOnClickListener(view -> {
             BottomSheetDialog dialog = new BottomSheetDialog(requireContext());
-            View v = dialog.getLayoutInflater().inflate(R.layout.task_editor,null,true);
-            dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
-
-            dialog.setContentView(v);
-            dialog.show();
         });
         return binding.getRoot();
     }
