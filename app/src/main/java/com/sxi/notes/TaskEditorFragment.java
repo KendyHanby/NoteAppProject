785e805558f1d9bc0538bf1e0ee8532f5444557c
Kunteinzo <theinhtetoo473@gmail.com>
2023-02-13 13:43:18 +0630

    testing task editor

---
+++ app/src/main/java/com/sxi/notes/TaskEditorFragment.java
@@ -0,0 +1,77 @@
+package com.sxi.notes;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Rect;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.sxi.notes.databinding.FragmentTaskEditorBinding;
+
+public class TaskEditorFragment extends BottomSheetDialogFragment {
+
+    private FragmentTaskEditorBinding binding;
+
+    public TaskEditorFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentTaskEditorBinding.inflate(inflater,container,false);
+
+        binding.button.setOnClickListener(v->{
+            dismiss();
+        });
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        binding.getRoot().getViewTreeObserver().addOnGlobalLayoutListener(() -> {
+            Rect r = new Rect();
+            binding.getRoot().getWindowVisibleDisplayFrame(r);
+            int heightDiff = binding.getRoot().getRootView().getHeight() - (r.bottom - r.top);
+            if (heightDiff > dpToPx( requireContext(),200f)) {
+                binding.getRoot().setPadding(0, 0, 0, heightDiff - dpToPx( requireContext(),50f));
+            } else {
+                binding.getRoot().setPadding(0, 0, 0, 0);
+            }
+        });
+        /*getDialog().setOnShowListener(dialog -> {
+            BottomSheetDialog d = (BottomSheetDialog) dialog;
+            FrameLayout bottomSheet = d.findViewById(com.google.android.material.R.id.design_bottom_sheet);
+            BottomSheetBehavior behavior = BottomSheetBehavior.from(bottomSheet);
+            behavior.setPeekHeight(bottomSheet.getHeight());
+        });*/
+        getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+    }
+
+    private int dpToPx(Context context, float dp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics());
+    }
+
+}
